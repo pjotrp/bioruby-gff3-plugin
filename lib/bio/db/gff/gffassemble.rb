76e6cc07037de5a957714f86994de4ba88bc77d5
Pjotr Prins <pjotr.public01@thebird.nl>
2010-08-22 13:39:46 +0200

    Container types, and some error handling

--- lib/bio/db/gff/gffassemble.rb
+++ lib/bio/db/gff/gffassemble.rb
@@ -9,9 +9,9 @@
 
 module Bio
   module GFFbrowser
- 
+
     module Helpers
- 
+
       module Error
         def warn str,id=''
           $stderr.print "Warning: "+str+" <#{id}>\n"
@@ -25,20 +25,17 @@ module Bio
           self[id] += 1
         end
       end
-     
+
       # Helper class for storing linked records based on a shared ID
       class LinkedRecs < Hash
-        include Error 
+        include Error
         def add rec
           parent = rec.get_attribute('Parent')
-          if parent
-            id = rec.id
-            puts "Adding <#{id}>"
-            self[id] = [] if self[id] == nil
-            self[id] << rec
-          else
-            warn "record has no parent",id
-          end
+          id = rec.id
+          warn "record has no parent",id if !parent
+          puts "Adding #{rec.feature_type} <#{id}>"
+          self[id] = [] if self[id] == nil
+          self[id] << rec
         end
 
         def validate
@@ -62,50 +59,64 @@ module Bio
           super(rec.start,rec.end)
         end
         def intersection(other)
-          raise ArgumentError, 'value must be a Range' unless other.kind_of?(Range)  
-          min, max = first, exclude_end? ? max : last  
-          other_min, other_max = other.first, other.exclude_end? ? other.max : other.last  
-          new_min = self === other_min ? other_min : other === min ? min : nil  
-          new_max = self === other_max ? other_max : other === max ? max : nil  
-          new_min && new_max ? new_min..new_max : nil  
-        end  
-        alias_method :&, :intersection  
+          raise ArgumentError, 'value must be a Range' unless other.kind_of?(Range)
+          min, max = first, exclude_end? ? max : last
+          other_min, other_max = other.first, other.exclude_end? ? other.max : other.last
+          new_min = self === other_min ? other_min : other === min ? min : nil
+          new_max = self === other_max ? other_max : other === max ? max : nil
+          new_min && new_max ? new_min..new_max : nil
+        end
+        alias_method :&, :intersection
         def <=> other
           first <=> other.first
         end
-      end  
+      end
     end # Helpers
-     
+
     module MRNA
       include Helpers
       include Helpers::Error
+      attr_reader :genes, :contigs
 
-      IGNORE_FEATURES = %w{TF_binding_site intronSO:0000188 polyA_sequence SO:0000610 polyA_site SO:0000553 five_prime_UTR SO:0000204 three_prime_UTR SO:0000205}
+      CONTAINER_TYPES = %w{
+        gene SO:0000704 contig transcript Component region
+      }
+      # Ignore the following features (case sensitive?)
+      IGNORE_FEATURES = CONTAINER_TYPES + %w{
+        transposon Match similarity UTR
+        TF_binding_site intronSO:0000188 polyA_sequence SO:0000610
+        polyA_site SO:0000553
+        five_prime_UTR SO:0000204 three_prime_UTR SO:0000205
+      }
 
       # Digest mRNA from the GFFdb and store in Hash
       # Next yield(id, seq) from Hash
       def each_mRNA
         puts "---- Digest DB and store data in mRNA Hash"
-        ids   = Counter.new   # Count ids
-        seqnames = Counter.new   # Count seqnames
-        ignored_features = {}
-        genes = []   # Store gene ids
-        mrnas = LinkedRecs.new   # Store linked mRNA records
-        cdss  = LinkedRecs.new
+        count_ids       = Counter.new   # Count ids
+        count_seqnames  = Counter.new   # Count seqnames
+        containers            = {} # Store containers, like genes, contigs
+        mrnas                 = LinkedRecs.new   # Store linked mRNA records
+        cdss                  = LinkedRecs.new
+        unrecognized_features = {}
         @gffs.each do | gff |
           gff.records.each do | rec |
-            seqnames.add(rec.seqname)
             id = rec.id
-            ids.add(id)
-            case rec.feature_type
-              when 'gene' || 'SO:0000704' : genes << id 
-              when 'mRNA' || 'SO:0000234' : mrnas.add(rec) 
-              when 'CDS'  || 'SO:0000316' : cdss.add(rec)
-              when 'exon' || 'SO:0000147' : false
-              else
-                if !IGNORE_FEATURES.include?(rec.feature_type)
-                  ignored_features[rec.feature_type] = true
-                end
+            count_ids.add(id)
+            count_seqnames.add(rec.seqname)
+
+            if CONTAINER_TYPES.include?(rec.feature_type)
+              containers[id] = rec
+            else
+              case rec.feature_type
+                when 'mRNA' || 'SO:0000234' : mrnas.add(rec)
+                when 'CDS'  || 'SO:0000316' : cdss.add(rec)
+                when 'exon' || 'SO:0000147' : false
+                else
+                  if !IGNORE_FEATURES.include?(rec.feature_type)
+                    unrecognized_features[rec.feature_type] = true
+                  end
+              end
             end
           end
         end
@@ -118,10 +129,10 @@ module Bio
         cdss.each do | k, v |
           yield k,v
         end
- 
-        ignored_features.keys.each do | k |
-          warn "Feature has no match",k
+        unrecognized_features.keys.each do | k |
+          warn "Feature has no match",k if k
         end
+        return genes, contigs, mrnas, cdss
       end
     end
   end
