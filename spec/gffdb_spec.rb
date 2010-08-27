1bec2b8d7d7ce1072e3b5c0077be06912058132d
Pjotr Prins <pjotr.public01@thebird.nl>
2010-08-27 10:39:14 +0200

    Spec: factored out shared tests

--- spec/gffdb_spec.rb
+++ spec/gffdb_spec.rb
@@ -15,14 +15,7 @@ TEST_NON_IMPLEMENTED=false
 
 TEST1='test/data/gff/test.gff3'
 
-describe GFFdb, "GFF3 API with :cache_none" do
-
-  before :all do 
-    # initialize
-    gff3 = Bio::GFF::GFF3.new(File.read(TEST1))
-    @gffdb = Bio::GFFbrowser::GFFdb.new(gff3)
-  end
-
+def iterators_should_be_implemented
   if TEST_NON_IMPLEMENTED
     it "should implement each_gene" 
     it "should implement each_gene" 
@@ -36,7 +29,6 @@ describe GFFdb, "GFF3 API with :cache_none" do
     h = {} ; @gffdb.each_mRNA_seq { | id, seq | h[id] = seq }
     h["mrna01short Sequence:test01_1:400 (3:14)"].should == "GAAGATTTGTAT"
   end
-
   it "should implement each_CDS_seq" do
     h = {} ; @gffdb.each_CDS_seq { | id, seq | h[id] = seq }
     h.keys.sort[0].should == "cds1 Sequence:test01_1:400 (164:190, 192:200)"
@@ -44,46 +36,27 @@ describe GFFdb, "GFF3 API with :cache_none" do
   end
 end
 
-describe GFFdb, "GFF3 API with :cache_all" do
-  if TEST_NON_IMPLEMENTED
-    it "should implement each_gene" 
-    it "should implement each_gene" 
-    it "should implement each_gene_seq" 
-    it "should implement each_mRNA" 
-    it "should implement each_mRNA_seq" 
-    it "should implement each_exon" 
-    it "should implement each_exon_seq" 
-    it "should implement each_CDS" 
-    it "should implement each_CDS_seq" 
+describe GFFdb, "GFF3 API with :cache_none" do
+
+  before :all do 
+    # initialize
+    gff3 = Bio::GFF::GFF3.new(File.read(TEST1))
+    @gffdb = Bio::GFFbrowser::GFFdb.new(gff3)
   end
+
+  iterators_should_be_implemented
+end
+
+describe GFFdb, "GFF3 API with :cache_all" do
+  # iterators_should_be_implemented
 end
 
 describe GFFdb, "GFF3 API with :cache_component" do
-  if TEST_NON_IMPLEMENTED
-    it "should implement each_gene" 
-    it "should implement each_gene" 
-    it "should implement each_gene_seq" 
-    it "should implement each_mRNA" 
-    it "should implement each_mRNA_seq" 
-    it "should implement each_exon" 
-    it "should implement each_exon_seq" 
-    it "should implement each_CDS" 
-    it "should implement each_CDS_seq" 
-  end
+  # iterators_should_be_implemented
 end
 
 describe GFFdb, "GFF3 API with external FASTA" do
-  if TEST_NON_IMPLEMENTED
-    it "should implement each_gene" 
-    it "should implement each_gene" 
-    it "should implement each_gene_seq" 
-    it "should implement each_mRNA" 
-    it "should implement each_mRNA_seq" 
-    it "should implement each_exon" 
-    it "should implement each_exon_seq" 
-    it "should implement each_CDS" 
-    it "should implement each_CDS_seq" 
-  end
+  # iterators_should_be_implemented
 end
 
 
