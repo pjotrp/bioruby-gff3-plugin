df0d6e353f98197f35c5a5f41f8b1354085c4b55
Pjotr Prins <pjotr.public01@thebird.nl>
2011-01-02 17:36:05 +0100

    Added --translate and --validate switches

--- lib/bio/output/gfffastawriter.rb
+++ lib/bio/output/gfffastawriter.rb
@@ -3,20 +3,29 @@ module Bio
   module GFFbrowser
 
     class FastaWriter
-      def initialize translate
+      def initialize translate, validate
         @do_translate = translate
+        @do_validate = validate
       end
 
       def put id, seq
         puts '>'+id
-        if @do_translate
-          ntseq = Bio::Sequence::NA.new(seq)
-          puts ntseq.translate
-        else
-          puts seq
-        end
+        put_seq seq
       end
+      private
 
+      def put_seq seq
+        if @do_translate or @do_validate
+          ntseq = Bio::Sequence::NA.new(seq)
+          aaseq = ntseq.translate
+          puts aaseq if @do_translate
+          if @do_validate
+            raise 'Validation problem of '+id if aaseq.count('*') > 1
+          end
+          return if @do_translate
+        end
+        puts seq
+      end
     end
   end
 end
