3be808f97d48d93af2cfb2b32694e287c8541a80
Pjotr Prins <pjotr.public01@thebird.nl>
2011-01-11 14:02:15 +0100

    GFF Record adapter

--- spec/gffparserec.rb
+++ spec/gffparserec.rb
@@ -25,6 +25,20 @@ describe FastLineParser, "GFF3 Fast line parser" do
   it "should parse records" do 
     parse_line_fast("ctg123\t.\tCDS\t1201\t1500\t.\t+\t0\tID=cds00001;Parent=mRNA00001;Name=edenprotein.1").should ==  ["ctg123", ".", "CDS", 1201, 1500, 0.0, "+", 0, {"ID"=>"cds00001", "Parent"=>"mRNA00001", "Name"=>"edenprotein.1"}]
   end
+  it "should handle a Record interface" do
+    fields = 
+    parse_line_fast("ctg123\t.\tCDS\t1201\t1500\t.\t+\t0\tID=cds00001;Parent=mRNA00001;Name=edenprotein.1")
+    rec = Bio::GFFbrowser::FastLineRecord.new(fields)
+    rec.seqname.should == "ctg123"
+    rec.id.should == "cds00001"
+    rec.phase.should == 0
+    rec.start.should == 1201
+    rec.end.should == 1500
+    rec.score.should == 0.0
+    rec.strand.should == '+'
+    rec.feature.should == 'CDS'
+    rec.source.should == '.'
+  end
 end
 
 
